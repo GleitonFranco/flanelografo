8982abadb73e3159729fcd8a60bda42601ed13f4
GleitonFranco <gleitonfranco@gmail.com>
2013-06-12 08:05:34 -0300

    Update FlanelografoService.java

--- src/main/java/br/edu/fanor/flanelografo/webservice/FlanelografoService.java
+++ src/main/java/br/edu/fanor/flanelografo/webservice/FlanelografoService.java
@@ -4,11 +4,13 @@ import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebService;
 
-@WebService()
-public class FlanelografoService {
-	
-	@WebMethod(operationName="consultaSala")
-	public String consultaSala(@WebParam(name="curso") String curso) {
-		return "Lab-Inf-4";
+@SOAPBinding(style = SOAPBinding.Style.DOCUMENT, use = SOAPBinding.Use.LITERAL, parameterStyle = SOAPBinding.ParameterStyle.WRAPPED)
+@WebService(name="flanelografo",targetNamespace="http://localhost:8180/tcembed/webservice")
+	public class Flanelografo {
+		
+		@WebMethod(operationName="consultaSala")
+		public String consultaSala(@WebParam(name="curso") String curso) {
+			return "Lab-Inf-4";
+		}
 	}
 }
